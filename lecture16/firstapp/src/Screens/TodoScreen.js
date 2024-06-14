cf1959d228f657a6fb858ed10b70e701b986b5ab
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-06-14 23:40:00 +0530

    Delete lecture16/firstapp directory

--- lecture16/firstapp/src/Screens/TodoScreen.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-class TodoScreen extends Component{
-    render(){
-        return(
-            <h1 className="ui-heading center">To Do List</h1>
-        )
-    }
-}
-export default TodoScreen;
