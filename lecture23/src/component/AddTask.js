61e74bdc4312843e7471d780b2d1ad936c9ebfe8
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-06-17 20:14:49 +0530

    Add files via upload

--- lecture23/src/component/AddTask.js
+++ lecture23/src/component/AddTask.js
@@ -1,1 +1,57 @@
+import { useState } from "react";
 
+const AddTask = () => {
+    const [task, setTask] = useState({
+        title: "",
+        description: "",
+    });
+
+    const handleInputChange = (e) => {
+        setTask({
+            ...task, [e.target.name]: e.target.value,
+        });
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(task);
+        
+    }
+//    const refresh = (e) =>{
+//     window.location.reload();
+//    }
+    return (
+        <>
+            <h3 className="ui heading center">Add New Task</h3>
+            <div className="ui form">
+                <form onSubmit={onFormSubmit}>
+                    <div className="field">
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            spellCheck={false}
+                            placeholder="Task Title"
+                            name="title"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <label>Description</label>
+                        <textarea
+                            rows="2"
+                            spellCheck={false}
+                            placeholder="Description"
+                            name="description"
+                            onChange={handleInputChange}
+                        ></textarea>
+                    </div>
+                    <button type="submit" className="ui primary button" >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default AddTask;
