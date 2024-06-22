2c05dae22ff9ad0b207084c149fb4fd2026d99c0
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-06-22 20:48:38 +0530

    Upload files via upload

---
+++ lecture25/src/Components/AddTask.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const AddTask = ({onSubmit}) => {
+    const [task, setTask] = useState({
+        title: "",
+        description: "",
+    });
+
+    const handleInputChange = (e) => {
+        setTask({
+            ...task, [e.target.name]: e.target.value,
+        });
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(task);
+        onSubmit(task);
+        
+    }
+//    const refresh = (e) =>{
+//     window.location.reload();
+//    }
+    return (
+        <>
+        <section className="screen">
+            <h3 className="ui heading center">Add New Task</h3>
+            <div className="ui form">
+                <form onSubmit={onFormSubmit}>
+                    <div className="field">
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            spellCheck={false}
+                            placeholder="Task Title"
+                            name="title"
+                            onChange={handleInputChange}
+                            value = {task.title}
+                        />
+                    </div>
+                    <div className="field">
+                        <label>Description</label>
+                        <textarea
+                            rows="2"
+                            spellCheck={false}
+                            placeholder="Description"
+                            name="description"
+                            onChange={handleInputChange}
+                            value = {task.description}
+                        ></textarea>
+                    </div>
+                    <button type="submit" className="ui primary button" >
+                        Submit
+                    </button>
+                </form>
+            </div>
+            </section>
+        </>
+    );
+}
+
+export default AddTask;
