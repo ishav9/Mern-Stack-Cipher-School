f0fe91d917498f25f60901477fe8a703b1ca50ae
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-06-22 20:47:51 +0530

    Upload files via upload

---
+++ lecture25/src/Screens/TodoScreen.js
@@ -0,0 +1,45 @@
+
+
+import {useState} from "react";
+import Task from "../Component/task";
+import AddTask from "../Component/AddTask";
+
+
+
+const TodoScreen = () => {
+    const  [taskList, settaskList] = useState([]);
+    let addNewTask = (task) => {
+        settaskList([...taskList, {...task, createDate: new Date()}]);
+    };
+    return(
+        <>
+        <div className="screen">         
+        <h1 className="ui-heading center">To Do List</h1>      
+               <div>
+                       <button onClick={(e)=>{
+                        settaskList([...taskList,{
+                            title:"Go to gym",
+                            discription: "Going to gym is good for muscle grow",
+                            createDate: new Date(),
+                        },
+                    ]);
+                            
+                    }} 
+                    className="ui secondary button">Add Task</button>
+                    <section>
+                    <div className="ui cards">
+                     {taskList.map((task, index)=>(
+                                   <Task task={task} keys={index} />
+                            ))
+                    }        
+                    </div>
+                    </section>
+                    {/* <AddTask onSubmit ={addNewTask}
+                     /> */}
+                    </div>
+                    </div>
+                    
+                    </>
+    )
+}
+export default TodoScreen;
