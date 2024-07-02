e3d36e439aa9c6e1007171672f255c69e89166ac
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-07-02 14:36:01 +0530

    Add files via upload

---
+++ lecture27-todolist/src/App.js
@@ -0,0 +1,32 @@
+// import { Component } from "react";
+import { useContext, useState } from "react";
+import AddTask from "./Component/AddTask";
+import TodoScreen from "./Screens/TodoScreen";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import TaskContext from "./context/TaskContext";
+import { TaskProvider } from "./context/TaskContext";
+
+const router = createBrowserRouter([
+    {
+    path:"/",
+    element:<TodoScreen/> ,// the page u want to render
+},
+ {
+    path:"/add-task",
+    element:<AddTask/>,
+ }
+])
+
+const App = ()=>{
+    // const {taskList , addnewTask} = useContext(TaskContext);
+    
+    // return <RouterProvider router = {router} />;
+    return (
+      <TaskProvider>
+        <RouterProvider router = {router}/>
+      </TaskProvider>
+    )
+}
+export default App;
+
+// CDN stands for content Distribution Network
