df88f29124dae1376b9004c44ed7c30559fb0828
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-07-08 22:47:15 +0530

    Add files via upload

---
+++ lecture47/src/jwt.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const CS_SECRET_KEY = "CSSecretKey";
+const generateToken = (payload)=>{
+    const token = jwt.sign(payload, CS_SECRET_KEY);
+    return token;
+}
+const verifyToken = (token)=>{
+    try{
+        const payload = jwt.verify(token, CS_SECRET_KEY);
+        return {isValideToken: true, payload};
+    }catch(err){
+        console.error(err);
+        return {isValideToken:false, payload:undefined};
+
+    }
+}
+module.exports ={generateToken, verifyToken};
+// console.log(generateToken({name:"Isha verma"}));
+// console.log(verifyToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSXNoYSB2ZXJtYSIsImlhdCI6MTcyMDQ1MjE0NH0.wkvduG74CeCQiNT5wU1rGU1ONGBffLiTAPFh9FROA3A"));
