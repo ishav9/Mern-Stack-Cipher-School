cc9d1291fa473c34c7fedd1ff77ff91aadfd7501
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-07-10 15:55:19 +0530

    Add files via upload

---
+++ lecture51/TaskApplication/bcrypt.js
@@ -0,0 +1,14 @@
+const bcrypt = require("bcrypt");
+const encryptPassword = async(plainTextPassword)=>{
+    try{
+        return await bcrypt.hash(plainTextPassword,8);
+    }catch(err){
+        console.error(err);
+        throw err;
+    }
+};
+ const checkPassword =  async(plainTextPassword, encryptedPassword)=>{
+    return bcrypt.compare(checkPassword, encryptPassword);
+
+ }
+ module.exports={checkPassword, encryptPassword};
