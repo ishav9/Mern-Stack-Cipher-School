22bc2ccb52bc7ca30f217af8ae72fac2dc098929
ishav9 <118092279+ishav9@users.noreply.github.com>
2024-07-10 15:51:04 +0530

    Add files via upload

--- lecture51/utils/DateUtil.js
+++ lecture51/utils/DateUtil.js
@@ -1,1 +1,13 @@
+const dateFormat = new Intl.DateTimeFormat("en-IN",{
+   
+    // dateStyle: "medium",
+    hour12:true,
+    hour:"numeric",
+    minute:"numeric",
+    year:"numeric",
+    month:"short",
+    day:"2-digit",
+}
+);
+export const formatDate = (date) => dateFormat.format(date);
 
---
+++ lecture51/utils/LoginUtil.js
@@ -0,0 +1,13 @@
+import { TaskApplicationBackend } from "../apis/TaskApplicationApis"
+import { loginUserApi } from "../apis/user-api"
+
+export const loginUser = async({email,password})=>{
+   const {token} = await loginUserApi({email, password});
+   localStorage.setItem("token", token);
+};
+export const getUserToken =()=> {
+    localStorage.getItem("token");
+}
+export const logoutUser = ()=>{
+    localStorage.removeItem("token");
+}
